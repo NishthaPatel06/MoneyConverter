b310e6a76fb629289c3ed7a65d4618707520f436
NishthaPatel06 <200474598@student.georgianc.on.ca>
2022-07-17 17:10:53 -0400

    anonymous users

--- MoneyConverter/Startup.cs
+++ MoneyConverter/Startup.cs
@@ -33,6 +33,7 @@ namespace MoneyConverter
             services.AddDatabaseDeveloperPageExceptionFilter();
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddAuthentication()
